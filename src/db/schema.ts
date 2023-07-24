2a80b7e00d4551934b43bbf3a18dd327c94ca552
Drew Harris <harrisd@smu.edu>
2023-07-23 21:59:48 -0600

    Update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,47 @@
 import { InferModel } from "drizzle-orm";
-import { mysqlTable, serial } from "drizzle-orm/mysql-core";
+import { bigint, int, mysqlTable, text, varchar } from "drizzle-orm/mysql-core";
 
-export const todos = mysqlTable("todos", {
-  id: serial("id").primaryKey(),
+export const podcast = mysqlTable("podcasts", {
+  id: int("id").autoincrement().primaryKey(),
+  title: varchar("title", {
+    length: 255,
+  }).notNull(),
 });
 
-export type Todo = InferModel<typeof todos>;
+export type Todo = InferModel<typeof podcast>;
+
+export const episode = mysqlTable("episodes", {
+  id: int("id").autoincrement().primaryKey(),
+  videoId: varchar("video_id", {
+    length: 64,
+  })
+    .notNull()
+    .unique(),
+  title: varchar("title", {
+    length: 255,
+  }).notNull(),
+  thumbnail: varchar("thumbnail", {
+    length: 255,
+  }),
+  podcastId: int("podcast_id").references(() => podcast.id, {
+    onDelete: "cascade",
+  }),
+});
+
+export type Episode = InferModel<typeof episode>;
+
+export const moment = mysqlTable("moments", {
+  id: int("id").autoincrement().primaryKey(),
+  content: text("content").notNull(),
+  timestamp: bigint("timestamp", {
+    mode: "bigint",
+  }),
+  episodeId: int("episode_id").references(() => episode.id, {
+    onDelete: "cascade",
+  }),
+  podcastId: int("podcast_id").references(() => podcast.id, {
+    onDelete: "cascade",
+  }),
+});
+
+export type Moment = InferModel<typeof moment>;
