508954146905861753359e4393460cf14430e592
Drew Harris <harrisd@smu.edu>
2023-11-27 15:24:49 -0600

    FIX!!!

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,10 +1,16 @@
 import Elysia from "elysia";
 
 export const analytics = new Elysia().derive((ctx) => {
-  const cookieKey =
-    "ph_phc_XUCFhANAnyrDWNV0T8zUtWpMM2wKcoFsao7z0Sz0hS4_posthog";
-  const header = ctx.cookie[cookieKey];
-  return {
-    distinct: (header.get().distinct_id as string) || undefined,
-  };
+  try {
+    const cookieKey =
+      "ph_phc_XUCFhANAnyrDWNV0T8zUtWpMM2wKcoFsao7z0Sz0hS4_posthog";
+    const header = ctx.cookie[cookieKey];
+    return {
+      distinct: (header.get().distinct_id as string) || undefined,
+    };
+  } catch (error) {
+    return {
+      distinct: undefined,
+    };
+  }
 });
