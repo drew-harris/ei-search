a0af2330bd740e1c3e6f9cb5ec06a10413ad682c
Drew Harris <harrisd@smu.edu>
2023-12-18 22:07:16 -0600

    Fix again???

--- src/analytics.ts
+++ src/analytics.ts
@@ -6,13 +6,13 @@ export const analytics = new Elysia().derive((ctx) => {
     const cookieKey =
       "ph_phc_XUCFhANAnyrDWNV0T8zUtWpMM2wKcoFsao7z0Sz0hS4_posthog";
 
-    if (!ctx.cookie[cookieKey]) {
+    if (!ctx?.cookie[cookieKey]) {
       return {
         distinct: undefined,
       };
     }
     const header = ctx.cookie[cookieKey];
-    if (!header.get().distinct_id) {
+    if (!header?.get()?.distinct_id) {
       return {
         distinct: undefined,
       };
