38f51d2f3e84ceb2832527272c216b3e42aa26d9
Drew Harris <harrisd@smu.edu>
2023-12-18 22:39:39 -0600

    Remove issues

--- src/index.tsx
+++ src/index.tsx
@@ -32,9 +32,6 @@ const app = new Elysia()
           </a>
           <Header />
           <div class="flex m-2 flex-col items-center">
-            <div class="font-bold">
-              Site is experiencing issues right now. Sorry.
-            </div>
             <div class="flex bg-white px-3 w-full border border-black mt-5 md:w-[300px] rounded-md">
               <input
                 name="q"
