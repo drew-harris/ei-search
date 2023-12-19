51d793c14523dca58b2578856a1730399b96555e
Drew Harris <harrisd@smu.edu>
2023-12-18 21:55:33 -0600

    Fix error

--- src/analytics.ts
+++ src/analytics.ts
@@ -5,11 +5,23 @@ export const analytics = new Elysia().derive((ctx) => {
   try {
     const cookieKey =
       "ph_phc_XUCFhANAnyrDWNV0T8zUtWpMM2wKcoFsao7z0Sz0hS4_posthog";
+
+    if (!ctx.cookie[cookieKey]) {
+      return {
+        distinct: undefined,
+      };
+    }
     const header = ctx.cookie[cookieKey];
+    if (!header.get().distinct_id) {
+      return {
+        distinct: undefined,
+      };
+    }
     return {
-      distinct: (header.get().distinct_id as string) || undefined,
+      distinct: header.get().distinct_id as string,
     };
   } catch (error) {
+    console.error(error);
     sentry.captureException(error);
     return {
       distinct: undefined,
